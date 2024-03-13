6640436f7a7f4e958009fce43db731ad203e094b
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2024-03-13 20:07:05 +0300

    init commit

---
+++ HeadHunter/HeadHunter/HeadHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadHunterApp.swift
+//  HeadHunter
+//
+//  Created by Василий Вырвич on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct HeadHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
